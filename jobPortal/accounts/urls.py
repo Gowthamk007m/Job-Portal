011a0b0d06dab18bbe66afb33706029376d7ccf9
Gowtham K M <gowthamk007m@gmail.com>
2024-08-23 20:04:43 +0530

    dgfh

--- jobPortal/accounts/urls.py
+++ jobPortal/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from .views import *
 from jobs.views import JobProfileSelectView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
 # router = DefaultRouter()
@@ -11,19 +11,22 @@ app_name = 'accounts'
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot_password'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('details/create/<id>', DetailsCreateView.as_view(), name='create_details'),    
-    path('activities/create/', ActivitiesCreateView.as_view(), name='create_activities'),    
-    path('qualifications/create/', QualificationsCreateView.as_view(), name='create_qualifications'),   
-    path('role/', JobProfileSelectView.as_view(), name='role'),    
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('register/', RegisterView.as_view(), name='register'),
+    # path('forgot-password/', ForgotPasswordView.as_view(), name='forgot_password'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('details/create/<id>', DetailsCreateView.as_view(), name='create_details'),    
+    # path('activities/create/', ActivitiesCreateView.as_view(), name='create_activities'),    
+    # path('qualifications/create/', QualificationsCreateView.as_view(), name='create_qualifications'),   
+    # path('role/', JobProfileSelectView.as_view(), name='role'),    
 
-    path('api/register/', RegisterAPIView.as_view(), name='api-register'),
-    path('api/login/',LoginAPIView.as_view(), name="api-login"),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/register/', RegisterAPIView.as_view(), name='api-register'),
+    # path('api/login/',LoginAPIView.as_view(), name="api-login"),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/login/google/', google_login, name='google_token'),
+
 
     
     # path('', include(router.urls)),
--- jobPortal/accounts/views.py
+++ jobPortal/accounts/views.py
@@ -1,184 +1,228 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render
-from django.urls import reverse, reverse_lazy
-from django.views.generic import TemplateView
-from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import authenticate, login
-from.mixin import *
-from .models import *
-from .forms import CustomUserCreationForm, LoginForm,ActivitiesForm,QualificationsForm,DetailsForm
-from django.contrib import messages
-from django.views.generic import CreateView,UpdateView,ListView,TemplateView
-from django.views import View
-from django.contrib.auth import logout 
+# from django.http import HttpResponseRedirect
+# from django.shortcuts import redirect, render
+# from django.urls import reverse, reverse_lazy
+# from django.views.generic import TemplateView
+# from django.contrib.messages.views import SuccessMessageMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth import authenticate, login
+# from.mixin import *
+# from .models import *
+# from .forms import CustomUserCreationForm, LoginForm,ActivitiesForm,QualificationsForm,DetailsForm
+# from django.contrib import messages
+# from django.views.generic import CreateView,UpdateView,ListView,TemplateView
+# from django.views import View
+# from django.contrib.auth import logout 
 
-#####API Imports########################
-from rest_framework import routers, serializers, viewsets
-from . serializers import *
+# #####API Imports########################
+# from rest_framework import routers, serializers, viewsets
+# from . serializers import *
 
-class RegisterView(SuccessMessageMixin, CreateView):
-    template_name = 'auth/register.html'
-    form_class = CustomUserCreationForm
+# class RegisterView(SuccessMessageMixin, CreateView):
+#     template_name = 'auth/register.html'
+#     form_class = CustomUserCreationForm
 
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        return HttpResponseRedirect(reverse('accounts:create_details', kwargs={'id': user.id}))
-
-
-class LoginView(View):
-    form_class = LoginForm
-    template_name = 'auth/login.html'
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'form': self.form_class()})
-
-    def post(self, request):
-        form = self.form_class(data=request.POST)
-
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=email, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('jobs:home')
-            else:
-                form.add_error(None, "Invalid email or password.")
-                return render(request, self.template_name, {'form': form})
+#     def form_valid(self, form):
+#         user = form.save()
+#         login(self.request, user)
+#         return HttpResponseRedirect(reverse('accounts:create_details', kwargs={'id': user.id}))
 
 
-class ForgotPasswordView(TemplateView):
-    template_name = 'auth/forgot_password.html'
+# class LoginView(View):
+#     form_class = LoginForm
+#     template_name = 'auth/login.html'
+
+#     def get(self, request, *args, **kwargs):
+#         return render(request, self.template_name, {'form': self.form_class()})
+
+#     def post(self, request):
+#         form = self.form_class(data=request.POST)
+
+#         if form.is_valid():
+#             email = form.cleaned_data.get('email')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, username=email, password=password)
+
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('jobs:home')
+#             else:
+#                 form.add_error(None, "Invalid email or password.")
+#                 return render(request, self.template_name, {'form': form})
+
+
+# class ForgotPasswordView(TemplateView):
+#     template_name = 'auth/forgot_password.html'
 
 
 
-class LogoutView(View):
-    def get(self, request):
-        logout(request)
-        return redirect('accounts:login')
+# class LogoutView(View):
+#     def get(self, request):
+#         logout(request)
+#         return redirect('accounts:login')
 
 
-class DetailsCreateView(LoginRequiredMixin, UpdateView):
-    model=CustomUser
-    form_class = DetailsForm
-    template_name = 'users/complete_profile.html'
-    success_url = reverse_lazy('accounts:create_activities')
-    pk_url_kwarg = 'id'
+# class DetailsCreateView(LoginRequiredMixin, UpdateView):
+#     model=CustomUser
+#     form_class = DetailsForm
+#     template_name = 'users/complete_profile.html'
+#     success_url = reverse_lazy('accounts:create_activities')
+#     pk_url_kwarg = 'id'
 
-    def get_queryset(self):
-        return CustomUser.objects.filter(username=self.request.user.username)
+#     def get_queryset(self):
+#         return CustomUser.objects.filter(username=self.request.user.username)
     
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super().form_valid(form)
 
-class ActivitiesCreateView(LoginRequiredMixin, CreateView):
-    form_class = ActivitiesForm
-    template_name = 'users/user_activities.html'
-    success_url = reverse_lazy('accounts:create_qualifications')
+# class ActivitiesCreateView(LoginRequiredMixin, CreateView):
+#     form_class = ActivitiesForm
+#     template_name = 'users/user_activities.html'
+#     success_url = reverse_lazy('accounts:create_qualifications')
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super().form_valid(form)
     
-class QualificationsCreateView(LoginRequiredMixin, CreateView):
-    form_class = QualificationsForm
-    template_name = 'users/user_qualifications.html'
-    success_url = reverse_lazy('accounts:role')
+# class QualificationsCreateView(LoginRequiredMixin, CreateView):
+#     form_class = QualificationsForm
+#     template_name = 'users/user_qualifications.html'
+#     success_url = reverse_lazy('accounts:role')
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-    
-
-# class UserHobbyUpdateView(UpdateView):
-#     model = UserHobby
-#     form_class = UserHobbyForm
-#     template_name = 'user_hobby_form.html'
-#     context_object_name = 'user_hobby'
-
-#     def get_object(self):
-#         user_id = self.kwargs.get('user_id')
-#         user_hobby, created = UserHobby.objects.get_or_create(user_id=user_id)
-#         return user_hobby
-
-#     def get_success_url(self):
-#         return reverse_lazy('some_view_name')
-
-class Role(LoginRequiredMixin, TemplateView):
-    template_name = 'users/role.html'
-
-#///////////////////////////////////API VIEWS//////////////////////////#
-
-# class LoginAPIView(viewsets.ModelViewSet):
-#     queryset=CustomUser.objects.all()
-#     serializer_class = CustomUserSerializer
-
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import CustomUserSerializer
-from rest_framework.permissions import AllowAny
-
-class RegisterAPIView(APIView):
-    permission_classes = [AllowAny]
-    def post(self, request, *args, **kwargs):
-        serializer = CustomUserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "user_id": user.id,
-                "email": user.email,
-                "access_token": str(refresh.access_token),
-                "refresh_token": str(refresh)
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super().form_valid(form)
     
 
-class LoginAPIView(APIView):
-    permission_classes = [AllowAny]
+# # class UserHobbyUpdateView(UpdateView):
+# #     model = UserHobby
+# #     form_class = UserHobbyForm
+# #     template_name = 'user_hobby_form.html'
+# #     context_object_name = 'user_hobby'
 
-    def post(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        password = request.data.get('password')
+# #     def get_object(self):
+# #         user_id = self.kwargs.get('user_id')
+# #         user_hobby, created = UserHobby.objects.get_or_create(user_id=user_id)
+# #         return user_hobby
 
-        # Authenticate the user
-        user = authenticate(request, username=email, password=password)
+# #     def get_success_url(self):
+# #         return reverse_lazy('some_view_name')
 
-        if user is not None:
-            # If authentication is successful, generate tokens
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "user_id": user.id,
-                "email": user.email,
-                "access_token": str(refresh.access_token),
-                "refresh_token": str(refresh)
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-        
+# class Role(LoginRequiredMixin, TemplateView):
+#     template_name = 'users/role.html'
 
-@api_view(['POST'])
+# #///////////////////////////////////API VIEWS//////////////////////////#
+
+# # class LoginAPIView(viewsets.ModelViewSet):
+# #     queryset=CustomUser.objects.all()
+
+# # class RegisterAPIView(APIView):
+# #     permission_classes = [AllowAny]
+# #     def post(self, request, *args, **kwargs):
+# #         serializer = CustomUserSerializer(data=request.data)
+# #         if serializer.is_valid():
+# #             user = serializer.save()
+# #             refresh = RefreshToken.for_user(user)
+# #             return Response({
+# #                 "user_id": user.id,
+# #                 "email": user.email,
+# #                 "access_token": str(refresh.access_token),
+# #                 "refresh_token": str(refresh)
+# #             }, status=status.HTTP_201_CREATED)
+# #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# # class LoginAPIView(APIView):
+# #     permission_classes = [AllowAny]
+
+# #     def post(self, request, *args, **kwargs):
+# #         email = request.data.get('email')
+# #         password = request.data.get('password')
+
+# #         # Authenticate the user
+# #         user = authenticate(request, username=email, password=password)
+
+# #         if user is not None:
+# #             # If authentication is successful, generate tokens
+# #             refresh = RefreshToken.for_user(user)
+# #             return Response({
+# #                 "user_id": user.id,
+# #                 "email": user.email,
+# #                 "access_token": str(refresh.access_token),
+# #                 "refresh_token": str(refresh)
+# #             }, status=status.HTTP_200_OK)
+# #         else:
+# #             return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+from django.http import JsonResponse
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from oauth2_provider.models import AccessToken, RefreshToken, Application
+from oauth2_provider.settings import oauth2_settings
+from oauthlib.common import generate_token
+from django.utils import timezone
+import datetime
+import json
+from social_django.models import UserSocialAuth
+from django.conf import settings
+
+@csrf_exempt
 def google_login(request):
-    token = request.data.get('token')
-    if not token:
-        return Response({'error': 'No token provided'}, status=400)
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            token = data.get('token')
+            
+            # Verify the token using Google API
+            id_info = id_token.verify_oauth2_token(token, google_requests.Request(), settings.GOOGLE_CLIENT_ID)
 
-    user = authenticate(request, token=token)
-    if user:
-        # Generate JWT or perform any other necessary steps
-        return Response({'token': 'YOUR_JWT_TOKEN'})
-    return Response({'error': 'Authentication failed'}, status=401)
+            # Get user info from token
+            email = id_info.get('email')
+            name = id_info.get('name')
 
-def authenticate(request, token):
-    try:
-        backend = 'social_core.backends.google.GoogleOAuth2'
-        user = psa(backend)(request)
-        return user
-    except Exception as e:
-        return None
+            # Use the custom user model
+            User = get_user_model()
+            user, created = User.objects.get_or_create(email=email, defaults={'username': email, 'first_name': name})
+
+            social_user, social_created = UserSocialAuth.objects.get_or_create(
+                user=user,
+                provider='google-oauth2',
+                uid=id_info['sub'],  # Use the 'sub' field from the ID token as UID
+                defaults={'extra_data': id_info}
+        )
+
+            # Generate tokens using Django OAuth Toolkit
+            application = Application.objects.get(name='JobPortal')  # Use your application name
+
+            # Create access token
+            expires = timezone.now() + datetime.timedelta(seconds=oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS)
+            access_token = AccessToken.objects.create(
+                user=user,
+                application=application,
+                token=generate_token(),
+                expires=expires,
+                scope='read write'
+            )
+
+            # Create refresh token
+            refresh_token = RefreshToken.objects.create(
+                user=user,
+                token=generate_token(),
+                application=application,
+                access_token=access_token
+            )
+
+            # Return user info and tokens
+            return JsonResponse({
+                'message': 'Login successful',
+                'user_id': user.id,
+                'email': user.email,
+                'access_token': access_token.token,
+                'refresh_token': refresh_token.token
+            })
+        except ValueError as e:
+            # Invalid token
+            return JsonResponse({'error': 'Invalid token'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
